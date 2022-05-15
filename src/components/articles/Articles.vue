96c2fdf974851b2f7cf3d7e64ffa1ba0a280a221
Mr.黄 <1106720577@qq.com>
2022-05-15 21:34:37 +0800

    调整

---
+++ src/components/articles/Articles.vue
@@ -0,0 +1,59 @@
+<script lang="ts" setup>
+import { ElDivider } from 'element-plus';
+import { ConstantArticle } from '@/constant/blog';
+import { getTime } from '@/utils/index';
+import { IconsEnum } from '@/constant/enum';
+import { isNull } from 'lodash';
+import TS from './TS.vue';
+import CSS from './CSS.vue';
+
+// 打开文章
+const handleOpenArticle = (path: string | null) => {
+  if (isNull(path)) return;
+  open(`https://super-zihao.github.io/learning/#/article/${path}`);
+};
+</script>
+
+<template>
+  <div v-for="(item, index) in ConstantArticle" :class="`relative item radius-12 p-8 ${index > 0 ? 'mt-16' : ''}`" :key="item.title">
+    <div :class="`${isNull(item.path) ? 'cd' : 'cp'}`" @click="handleOpenArticle(item.path)">
+      <div class="flex items-center fs-12 color-0">
+        <div>{{ getTime(item.time) }}</div>
+        <ElDivider v-if="item.subtitle" direction="vertical" />
+        <div v-if="item.subtitle">{{ item.subtitle }}</div>
+      </div>
+      <div class="mt-8 mb-12 fw-bold fs-18">
+        {{ item.title }}
+      </div>
+      <div class="flex items-center justify-between">
+        <div class="synopsis">
+          <div class="text">{{ item.synopsis }}</div>
+        </div>
+        <TS v-if="item.icon === IconsEnum.TYPESCRIPT" />
+        <CSS v-if="item.icon === IconsEnum.CSS" />
+      </div>
+    </div>
+    <div class="mt-12" v-if="item.front.length > 0">
+      <span>前置：</span>
+      <span v-for="(front, frontIndex) in item.front" :key="front.path">
+        <ElDivider v-if="frontIndex > 0" direction="vertical" />
+        <a :href="front.path" target="_blank">{{ front.title }}</a>
+      </span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.item {
+  width: 100%;
+  background-color: var(--bg-color);
+}
+.synopsis {
+  width: calc(100% - 116px);
+  .text {
+    text-indent: 2rem;
+    font-size: 14px;
+    color: #686868;
+  }
+}
+</style>
--- src/views/黄金屋.vue
+++ src/views/黄金屋.vue
@@ -1,11 +1,8 @@
 <script lang="ts" setup>
-import { ElDivider } from 'element-plus';
-import { BlogModule, Article, ListType } from '@/constant/blog';
-import { BlogTitleEnum, IconsEnum } from '@/constant/enum';
-import { getTime, handleString } from '@/utils/index';
-import TS from '@/components/TS.vue';
-import CSS from '../components/CSS.vue';
-import { isNull } from 'lodash';
+import { BlogModule, ListType } from '@/constant/blog';
+import { BlogTitleEnum } from '@/constant/enum';
+import { handleString } from '@/utils/index';
+import Article from '@/components/articles/Articles.vue';
 
 // 左侧打开链接
 const handleGoPath = (path: string | undefined, title: string, list: ListType) => {
@@ -17,12 +14,6 @@ const handleGoPath = (path: string | undefined, title: string, list: ListType) =
   }
   if (!path) return;
   open(path);
-};
-
-// 打开文章
-const handleOpenArticle = (path: string | null) => {
-  if (isNull(path)) return;
-  open(`https://super-zihao.github.io/learning/#/article/${path}`);
 };
 </script>
 
@@ -43,32 +34,9 @@ const handleOpenArticle = (path: string | null) => {
       </div>
     </aside>
     <div class="main">
-      <div v-for="(item, index) in Article" :class="`relative item radius-12 p-8 ${index > 0 ? 'mt-16' : ''}`" :key="item.title">
-        <div :class="`${isNull(item.path) ? 'cd' : 'cp'}`" @click="handleOpenArticle(item.path)">
-          <div class="flex items-center fs-12 color-0">
-            <div>{{ getTime(item.time) }}</div>
-            <ElDivider v-if="item.subtitle" direction="vertical" />
-            <div v-if="item.subtitle">{{ item.subtitle }}</div>
-          </div>
-          <div class="mt-8 mb-12 fw-bold fs-18">
-            {{ item.title }}
-          </div>
-          <div class="flex items-center justify-between">
-            <div class="synopsis">
-              <div class="text">{{ item.synopsis }}</div>
-            </div>
-            <TS v-if="item.icon === IconsEnum.TYPESCRIPT" />
-            <CSS v-if="item.icon === IconsEnum.CSS" />
-          </div>
-        </div>
-        <div class="mt-12" v-if="item.front.length > 0">
-          <span>前置：</span>
-          <span v-for="(front, frontIndex) in item.front" :key="front.path">
-            <ElDivider v-if="frontIndex > 0" direction="vertical" />
-            <a :href="front.path" target="_blank">{{ front.title }}</a>
-          </span>
-        </div>
-      </div>
+      <!-- 文章 -->
+      <Article />
+      <!-- 作品 -->
     </div>
   </div>
 </template>
@@ -103,14 +71,6 @@ const handleOpenArticle = (path: string | null) => {
   .item {
     width: 100%;
     background-color: var(--bg-color);
-  }
-  .synopsis {
-    width: calc(100% - 116px);
-    .text {
-      text-indent: 2rem;
-      font-size: 14px;
-      color: #686868;
-    }
   }
 }
 
